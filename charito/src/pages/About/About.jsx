8cb4a1345a1610249dea1a53e4a8efdb64da3824
StoleB <stole.boskov@gmail.com>
2025-04-02 13:34:07 +0200

    Update

--- charito/src/pages/About/About.jsx
+++ charito/src/pages/About/About.jsx
@@ -1,7 +1,12 @@
-import './About.css'
+import React from 'react';
 
-export const About = () => {
-    return (
-        <h1>About page</h1>
-    )
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About page.</p>
+    </div>
+  );
 }
+
+export default About;
